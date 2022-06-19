18f342956c06ee2166022d583e72d9a55bf79d66
Your Name <you@example.com>
2022-06-19 21:03:39 +0300

    ecommerce

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="App">My website</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
